b672db8fe99856d711bad086f106854c8583da63
RJ van der Weide <rj@rdcl.nl>
2015-08-09 01:16:15 +0200

    More efficient extraction of meta data.

--- src/parseMeta.js
+++ src/parseMeta.js
@@ -11,33 +11,16 @@ const Teensy = require('./Teensy');
 function parseMeta(data) {
     const defaultMeta = fs.readFileSync(path.join(Teensy.dirs.root, 'meta.yml'), 'utf8');
 
-    let parsed;
+    let parsed = {
+        meta: {},
+        template: data,
+    };
 
     if (data.startsWith('{{!')) {
-        let lines = data.substring('{{!'.length).split(/\r\n|\n|\r/);
+        let i = data.indexOf('}}');
         let meta = defaultMeta;
-
-        let line;
-        while ( (line = lines.shift()) != null ) {
-            if (line === '}}') {
-                break;
-            }
-
-            meta += line + '\n';
-        }
-
-        meta = yaml.safeLoad(meta).meta;
-
-        parsed = {
-            meta: meta,
-            template: lines.join('\n'),
-        };
-    }
-    else {
-        parsed = {
-            meta: {},
-            template: data,
-        };
+        meta += data.substring('{{!'.length, i);
+        parsed.meta = yaml.safeLoad(meta).meta;
     }
 
     return parsed;
