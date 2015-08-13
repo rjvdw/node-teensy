b82436ebc368fef0865e985ca5084f60d9006226
RJ van der Weide <rj@rdcl.nl>
2015-08-13 23:41:30 +0200

    Converted Teensy to nunjucks.

---
+++ src/nunjucksSetup.js
@@ -0,0 +1,176 @@
+"use strict";
+
+var util = require('util');
+
+var marked = require('marked');
+var moment = require('moment');
+var nunjucks = require('nunjucks');
+
+
+function nunjucksSetup(views) {
+    var env = new nunjucks.Environment(
+        new nunjucks.FileSystemLoader(views),
+        {
+            watch: true,
+            autoescape: true,
+        }
+    );
+
+    env.addFilter('date', function (value, format) {
+        return (
+            format == null
+                ? moment(value).format()
+                : moment(value).format(format)
+        );
+    });
+
+    env.addExtension('MarkdownParser', (function () {
+        var ext = {
+            tags: ['markdown'],
+
+            parse: function (parser, nodes, lexer) {
+                var tok = parser.nextToken();
+                var args = parser.parseSignature(null, true);
+                parser.advanceAfterBlockEnd(tok.value);
+
+                var body = parser.parseUntilBlocks('endmarkdown');
+                parser.advanceAfterBlockEnd();
+
+                return new nodes.CallExtension(ext, 'run', args, [body]);
+            },
+
+            run: function (context, body) {
+                var md = marked(body());
+
+                return new nunjucks.runtime.SafeString(md);
+            },
+        };
+
+        return ext;
+    })());
+
+    env.addExtension('MetaTags', (function () {
+        var ext = {
+            tags: ['meta'],
+
+            parse: function (parser, nodes, lexer) {
+                var tok = parser.nextToken();
+                var args = parser.parseSignature(null, true);
+                parser.advanceAfterBlockEnd(tok.value);
+
+                return new nodes.CallExtension(ext, 'run', args);
+            },
+
+            run: function (context, metaTags) {
+                var res = '';
+                var metaKeys = Object.keys(metaTags);
+                metaKeys.sort();
+
+                metaKeys.forEach(function (name) {
+                    var content = metaTags[name];
+
+                    res += util.format(
+                        '<meta name="%s" content="%s">\n',
+                        escape(name),
+                        escape(content)
+                    );
+                });
+
+                return new nunjucks.runtime.SafeString(res);
+            },
+        };
+
+        return ext;
+    })());
+
+    env.addExtension('Pagination', (function () {
+        var ext = {
+            tags: ['pagination'],
+
+            parse: function (parser, nodes, lexer) {
+                var tok = parser.nextToken();
+                var args = parser.parseSignature(null, true);
+                parser.advanceAfterBlockEnd(tok.value);
+
+                var body = parser.parseUntilBlocks('current');
+                parser.skipSymbol('current');
+                parser.skip(lexer.TOKEN_BLOCK_END);
+
+                var currentBody = parser.parseUntilBlocks('dotdot', 'endpagination');
+                var dotdotBody = null;
+
+                if (parser.skipSymbol('dotdot')) {
+                    parser.skip(lexer.TOKEN_BLOCK_END);
+                    dotdotBody = parser.parseUntilBlocks('endpagination');
+                }
+
+                parser.advanceAfterBlockEnd();
+
+                return new nodes.CallExtension(ext, 'run', args, [body, currentBody, dotdotBody]);
+            },
+
+            run: function (context, pagination, nrAround, body, currentBody, dotdotBody) {
+                var res = '';
+                var start = pagination.current - nrAround;
+                var end = pagination.current + nrAround;
+
+                // TODO: Is there a nicer way of doing this?
+                var pageNoBackup = context.ctx.pageNo;
+
+                if (start < 1) {
+                    end += 1 - start;
+                    start = 1;
+                }
+
+                if (end > pagination.lastPage) {
+                    start -= end - pagination.lastPage;
+                    end = pagination.lastPage;
+                }
+
+                if (start < 1) {
+                    start = 1;
+                }
+
+                if (dotdotBody && start > 1) {
+                    res += dotdotBody();
+                }
+
+                for (var i = start; i <= end; i++) {
+                    context.ctx.pageNo = i;
+
+                    if (i === pagination.current) {
+                        res += currentBody();
+                    }
+                    else {
+                        res += body();
+                    }
+                }
+
+                if (dotdotBody && end < pagination.lastPage) {
+                    res += dotdotBody();
+                }
+
+                context.ctx.pageNo = pageNoBackup;
+
+                return new nunjucks.runtime.SafeString(res);
+            },
+        };
+
+        return ext;
+    })());
+
+    return env;
+}
+
+// TODO: Doesn't nunjucks have its own escape function?
+function escape(str) {
+    str = str
+        .replace('&', '&amp;')
+        .replace('<', '&lt;')
+        .replace('>', '&gt;')
+        .replace('"', '&quot;');
+
+    return str;
+}
+
+exports = module.exports = nunjucksSetup;
--- test/Teensy.test.js
+++ test/Teensy.test.js
@@ -4,11 +4,13 @@ var http = require('http');
 var path = require('path');
 
 var expect = require('chai').expect;
+var koa = require('koa');
 var should = require('chai').should();
 
 var Teensy = require('../src/Teensy');
 
 var sampleAppDir = path.join(__dirname, 'sampleapp');
+var sampleAppWithout404Dir = path.join(__dirname, 'sampleapp_without404');
 
 
 describe('#Teensy', function () {
@@ -49,6 +51,126 @@ describe('#Teensy', function () {
 
                 res.on('end', function() {
                     expect(body).to.equal('<!DOCTYPE html>\n\n<h1>Test</h1>\n\n');
+                    server.close();
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should return a 404 response if the requested url does not exist', function (done) {
+        var server = Teensy(sampleAppDir).listen(function () {
+            var addr = server.address();
+
+            http.get({
+                host: addr.address,
+                port: addr.port,
+                method: 'GET',
+                path: '/does-not-exist',
+            }, function (res) {
+                should.exist(res);
+                expect(res).to.have.property('statusCode', 404);
+
+                res.setEncoding('utf8');
+                var body = '';
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+
+                res.on('end', function() {
+                    expect(body).to.equal('<h1>404</h1>\n');
+                    server.close();
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should return a generic 404 if the 404 template also does not exist', function (done) {
+        var server = Teensy(sampleAppWithout404Dir).listen(function () {
+            var addr = server.address();
+
+            http.get({
+                host: addr.address,
+                port: addr.port,
+                method: 'GET',
+                path: '/does-not-exist',
+            }, function (res) {
+                should.exist(res);
+                expect(res).to.have.property('statusCode', 404);
+
+                res.setEncoding('utf8');
+                var body = '';
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+
+                res.on('end', function() {
+                    expect(body).to.equal('Not Found');
+                    server.close();
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should do nothing if other middleware already gave a response', function (done) {
+        var app = koa();
+
+        app.use(Teensy(sampleAppDir));
+        app.use(function* (next) {
+            this.body = 'foo';
+            yield* next;
+        });
+
+        var server = app.listen(function () {
+            var addr = server.address();
+
+            http.get({
+                host: addr.address,
+                port: addr.port,
+                method: 'GET',
+                path: '/',
+            }, function (res) {
+                should.exist(res);
+                expect(res).to.have.property('statusCode', 200);
+
+                res.setEncoding('utf8');
+                var body = '';
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+
+                res.on('end', function() {
+                    expect(body).to.equal('foo');
+                    server.close();
+                    done();
+                });
+            });
+        });
+    });
+
+    it('requests other than GET an HEAD should be ignored', function (done) {
+        var server = Teensy(sampleAppDir).listen(function () {
+            var addr = server.address();
+
+            http.get({
+                host: addr.address,
+                port: addr.port,
+                method: 'POST',
+                path: '/',
+            }, function (res) {
+                should.exist(res);
+                expect(res).to.have.property('statusCode', 404);
+
+                res.setEncoding('utf8');
+                var body = '';
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+
+                res.on('end', function() {
+                    expect(body).to.equal('Not Found');
                     server.close();
                     done();
                 });
