258ad0f21fd74ba7e8c50d73e21cc7c06b7acb97
Ruud van der Weide <rj@rdcl.nl>
2015-09-09 07:52:18 +0200

    Updated travis settings and test scripts for Node.js 4.x compatibility.

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-node_or_iojs=`node -h | grep -il iojs > /dev/null && echo iojs || echo node`
-
-if [ "$node_or_iojs" = "node" ]; then
-    node --harmony_generators node_modules/.bin/istanbul cover _mocha -- -R spec test/*
-else
-    node node_modules/.bin/istanbul cover _mocha -- -R spec test/*
-fi
+node node_modules/.bin/istanbul cover _mocha -- -R spec test/*
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-node_or_iojs=`node -h | grep -il iojs > /dev/null && echo iojs || echo node`
-
-if [ "$node_or_iojs" = "node" ]; then
-    ./node_modules/.bin/mocha --harmony-generators --reporter spec
-else
-    ./node_modules/.bin/mocha --reporter spec
-fi
+./node_modules/.bin/mocha --reporter spec
